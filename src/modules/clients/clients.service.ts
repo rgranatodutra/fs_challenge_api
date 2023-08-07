7f7282aceda519068f4a88c088cb96411ee73335
Renan G. Dutra <r.granatodutra@gmail.com>
2023-08-06 23:01:07 -0300

    feat: add errors and validation

--- src/modules/clients/clients.service.ts
+++ src/modules/clients/clients.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 import { ClientsRepository } from './repositories/clients.abstract.repository';
+import { DeleteClientResponse } from './interfaces/delete.response.interface';
+import { Client } from './entities/client.entity';
 
 @Injectable()
 export class ClientsService {
@@ -10,7 +12,29 @@ export class ClientsService {
 	) { }
 
 	public async create(data: CreateClientDto) {
-		return await this.repository.create(data);
+		const findDuplicatedEmail = await this.repository.getMany({
+			email: data.email
+		});
+
+		if (findDuplicatedEmail.length) {
+			throw new ConflictException("Email already registered");
+		}
+
+		const findDuplicatedPhone = await this.repository.getMany({
+			phone: data.phone
+		});
+
+		if (findDuplicatedPhone.length) {
+			throw new ConflictException("Phone already registered");
+		}
+
+		const createdClient = await this.repository.create(data);
+
+		if (!createdClient) {
+			throw new InternalServerErrorException("Failed to create client");
+		}
+
+		return createdClient;
 	}
 
 	public async findAll() {
@@ -21,15 +45,30 @@ export class ClientsService {
 		return await this.repository.getMany(data);
 	}
 
-	public async findOne(id: number) {
-		return await this.repository.getOneById(id);
+	public async findOne(id: number): Promise<Client> {
+		const findClient = await this.repository.getOneById(id);
+
+		if (!findClient) {
+			throw new NotFoundException("Client not found");
+		}
+
+		return findClient;
 	}
 
 	public async update(id: number, data: UpdateClientDto) {
-		return await this.repository.update(id, data);
+		const findClient = await this.findOne(id);
+		const updatedClient = findClient && await this.repository.update(id, data);
+
+		return updatedClient;
 	}
 
-	public async remove(id: number) {
-		return await this.repository.delete(id);
+	public async remove(id: number): Promise<DeleteClientResponse> {
+		const findClient = await this.findOne(id);
+		await this.repository.delete(id);
+
+		return {
+			isDeleted: true,
+			deletedClient: findClient
+		}
 	}
 }
--- src/modules/clients/dto/create-client.dto.ts
+++ src/modules/clients/dto/create-client.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsPhoneNumber, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateClientDto {
+    @IsString()
+    @MinLength(10)
+    @MaxLength(120)
     fullName: string;
+
+    @IsEmail()
     email: string;
+
+    @IsPhoneNumber()
     phone: string;
 }
